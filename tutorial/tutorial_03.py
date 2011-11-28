72f2dde6ed8be24a6418a9553184df1d7a0f3d33
Stefan Urbanek <stefan@agentfarms.net>
2011-11-28 18:22:51 +0100

    updated tutorial 3

--- tutorial/tutorial_03.py
+++ tutorial/tutorial_03.py
@@ -28,11 +28,13 @@ FACT_VIEW = "vft_irbd_balance"
 
 engine = sqlalchemy.create_engine('sqlite:///:memory:')
 tutorial.create_table_from_csv(engine, 
-                      "data/IBRD_Balance_Sheet__FY2010-t02.csv", 
+                      "data/IBRD_Balance_Sheet__FY2010-t03.csv", 
                       table_name=FACT_TABLE, 
                       fields=[
-                            ("category", "string"), 
+                            ("category", "string"),
+                            ("category_label", "string"), 
                             ("subcategory", "string"), 
+                            ("subcategory_label", "string"), 
                             ("line_item", "string"),
                             ("year", "integer"), 
                             ("amount", "integer")],
@@ -40,7 +42,7 @@ tutorial.create_table_from_csv(engine,
                         
                         )
 
-model = cubes.load_model("models/model_02.json")
+model = cubes.load_model("models/model_03.json")
 
 cube = model.cube("irbd_balance")
 cube.fact = FACT_TABLE
@@ -53,33 +55,73 @@ dn = cubes.backends.sql.SQLDenormalizer(cube, connection)
 dn.create_view(FACT_VIEW)
 
 def drill_down(cell, dimension, path = []):
+    """Drill-down and aggregate recursively through als levels of `dimension`.
+    
+    This function is like recursively traversing directories on a file system and aggregating the
+    file sizes, for example.
+    
+    :Attributes:
+    * cell - cube cell to drill-down
+    * dimension - dimension to be traversed through all levels
+    * path - current path of the `dimension`
+    
+    Path is list of dimension points (keys) at each level. It is like file-system path.
+    """
+
+    # Get dimension's default hierarchy. Cubes supports multiple hierarchies, for example for
+    # date you might have year-month-day or year-quarter-month-day. Most dimensions will
+    # have one hierarchy, thought.
     hierarchy = dimension.default_hierarchy
+
+    # Can we go deeper in the hierarchy? Base path is path to the most detailed element,
+    # to the leaf of a tree, to the fact.
 
     if hierarchy.path_is_base(path):
         return
 
-
+    # Get the next level in the hierarchy. ``levels_for_pat`` returns list of levels
+    # according to provided path. When ``drilldown`` is set to ``True`` then one
+    # more level is returned.
+        
     levels = hierarchy.levels_for_path(path,drilldown=True)
     current_level = levels[-1]
 
-    level_label = dimension.attribute_reference(current_level.label_attribute)
+    # We need to know name of the level key attribute which contains a path component.
+    # If the model does not explicitly specify key attribute for the level, then first attribute
+    # will be used
     level_key = dimension.attribute_reference(current_level.key)
 
-    indent = "----" * len(path)
+    # For prettier display, we get name of attribute which contains label to be displayed
+    # for the current level. If there is no label attribute, then key attribute is used.
+    level_label = dimension.attribute_reference(current_level.label_attribute)
 
+    # Just visual formatting - indentation based on path lenght.
+    indent = "    " * len(path)
+
+    # We do the aggregation of the cell... Think of ``ls $CELL`` command in commandline, where `$CELL`
+    # is a directory name. In this function we can think of ``$CELL`` to be same as current working
+    # directory (pwd)
     result = browser.aggregate(cell, drilldown=[dimension])
 
-    for record in result.drilldown:
-        print "%s%s: count: %d amount: %d" % (indent, record[level_label], record["record_count"], record["amount_sum"])
+    # ... and display the results
+    # print "%s==Level: %s==" % (indent, current_level.label)
 
-        if not hierarchy.path_is_base(path):
-            drill_path = path[:] + [record[level_key]]
-            drill_down_cell = cell.slice(dimension, drill_path)
-            drill_down(drill_down_cell, dimension, drill_path)
+    for record in result.drilldown:
+        print "%s%s: %d" % (indent, record[level_label], record["record_count"])
+
+        # Construct new path: current path with key attribute value appended
+        drill_path = path[:] + [record[level_key]]
+
+        # Get a new cell slice for current path
+        drill_down_cell = cell.slice(dimension, drill_path)
+
+        # And do recursive drill-down
+        drill_down(drill_down_cell, dimension, drill_path)
 
 # Drill down through all levels of item hierarchy
 browser = cubes.backends.sql.SQLBrowser(cube, connection, view_name = FACT_VIEW)
 
+# Get whole cube
 cell = browser.full_cube()
 
 print "Drill down through all item levels:"
