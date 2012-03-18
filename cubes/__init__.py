3b1fb7dc4b8772c0b595256ca0a841ba1ebabc80
Stefan Urbanek <stefan@agentfarms.net>
2012-03-18 11:42:39 +0100

    namespace clean-up for #31

--- cubes/__init__.py
+++ cubes/__init__.py
@@ -12,19 +12,8 @@ from common import logger_name
 common._configure_logger()
 
 __all__ = [
-    "logger_name",
-    "load_model",
-    "model_from_path",
-    "cuts_from_string",
-    "string_from_cuts",
-    "attribute_list",
-    "Model",
-    "Cube",
-    "Dimension",
-    "Hierarchy",
-    "Level",
-    "AggregationBrowser",
-    "Cuboid",
-    "PointCut",
-    "MongoSimpleCubeBuilder",
+    "logger_name"
 ]
+
+__all__ += browser.__all__
+__all__ += model.__all__
