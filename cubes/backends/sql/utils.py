793645e436dec8851263682c28bd3f1316526297
Stefan Urbanek <stefan@agentfarms.net>
2012-04-24 19:28:06 +0200

    added JoinFinder, coalesce_physical; named tuples PhysicalReference, Join; all references are now triplets: schema, table, column
    change of references to triplets is for Issue #43 so we will be able to
    have tables in different database schemas

--- cubes/backends/sql/utils.py
+++ cubes/backends/sql/utils.py
@@ -82,3 +82,22 @@ def ddl_for_model(url, model, fact_prefix=None, dimension_prefix=None):
     metadata.create_all()
     
     return out.getvalue()
+
+def denormalize_locale(connection, localized, dernomralized, locales):
+    """Create denormalized version of localized table. (not imlpemented, just proposal)
+
+    Type 1:
+
+    Localized table: id, locale, field1, field2, ...
+
+    Denomralized table: id, field1_loc1, field1_loc2, field2_loc1, field2_loc2,...
+
+    Type 2:
+
+    Localized table: id, locale, key, field, content
+
+    Denomralized table: id, field1_loc1, field1_loc2, field2_loc1, field2_loc2,...
+
+
+    """
+    pass
