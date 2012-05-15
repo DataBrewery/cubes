153391ec940254611d8e612ec67c3d77adb94688
Stefan Urbanek <stefan@agentfarms.net>
2012-05-15 16:39:43 +0200

    Better handling of missing optional packages, also fixes #57

--- cubes/server/__init__.py
+++ cubes/server/__init__.py
@@ -1,5 +1,13 @@
-from slicer import Slicer, run_server
 from common import API_VERSION
+
+try:
+    from werkzeug import __version__ as werkzeug_version
+    from slicer import Slicer, run_server
+
+except ImportError:
+    from cubes.common import MissingPackage
+    _missing = MissingPackage("werkzeug", "Slicer server")
+    Slicer = run_server = _missing
 
 __all__ = (
     "Slicer",
