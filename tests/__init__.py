a1fdd6389440a8458fa43a020d1ca0db90030fb6
Stefan Urbanek <stefan@agentfarms.net>
2012-07-18 01:54:23 +0200

    more test file moving

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,25 +1,26 @@
 import unittest
 import os
 
-import model
-import browser
-import combinations
-import default_sql_backend
-import sql_star_browser
+# from .model import *
+# from .browser import *
+# from .combinations import *
+# from .default_sql_backend import *
+# from .sql_star_browser import *
 
-from cubes.common import get_logger
-import logging
 
-logger = get_logger()
-logger.setLevel(logging.DEBUG)
+# from cubes.common import get_logger
+# import logging
+# 
+# logger = get_logger()
+# logger.setLevel(logging.DEBUG)
 
-def suite():
-    suite = unittest.TestSuite()
-
-    suite.addTest(model.suite())
-    suite.addTest(browser.suite())
-    suite.addTest(combinations.suite())
-    suite.addTest(default_sql_backend.suite())
-    suite.addTest(sql_star_browser.suite())
-
-    return suite
+# def suite():
+#     suite = unittest.TestSuite()
+# 
+#     suite.addTest(model.suite())
+#     suite.addTest(browser.suite())
+#     suite.addTest(combinations.suite())
+#     suite.addTest(default_sql_backend.suite())
+#     suite.addTest(sql_star_browser.suite())
+# 
+#     return suite
