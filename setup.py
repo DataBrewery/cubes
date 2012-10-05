b93b23d2291c2e0e4f785c3d7858383ad78828b9
Stefan Urbanek <stefan@agentfarms.net>
2012-10-04 22:35:15 -0500

    bump version to 0.10

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.version_info < (2,7):
 
 setup(
     name = "cubes",
-    version = '0.9.1',
+    version = '0.10',
 
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine
