ad5c06db976065eeb593e91b84a0757da746bc02
Stefan Urbanek <stefan@agentfarms.net>
2012-10-31 15:23:38 -0400

    more cleanup

--- cubes/backends/sql/__init__.py
+++ cubes/backends/sql/__init__.py
@@ -2,6 +2,7 @@ from denormalizer import *
 from browser import *
 from star import *
 from mapper import *
+from workspace import *
 
 __all__ = []
 
@@ -9,3 +10,5 @@ __all__ += denormalizer.__all__
 __all__ += browser.__all__
 __all__ += star.__all__
 __all__ += mapper.__all__
+__all__ += workspace.__all__
+
--- cubes/backends/sql/star.py
+++ cubes/backends/sql/star.py
@@ -1,12 +1,11 @@
 # -*- coding=utf -*-
 from cubes.browser import *
 from cubes.common import get_logger
-from cubes.backends.sql.mapper import SnowflakeMapper, DenormalizedMapper
-from cubes.backends.sql.mapper import DEFAULT_KEY_FIELD
+from cubes.mapper import SnowflakeMapper, DenormalizedMapper
+from cubes.mapper import DEFAULT_KEY_FIELD
 import logging
 import collections
 from cubes.errors import *
-from .utils import CreateTableAsSelect, InsertIntoAsSelect
 from cubes.computation import *
 
 try:
@@ -26,8 +25,8 @@ except ImportError:
     aggregation_functions = {}
 
 __all__ = [
-    "StarBrowser",
-    "create_workspace"
+    "SnowflakeBrowser",
+    "QueryContext"
 ]
 
 # Required functionality checklist
@@ -59,13 +58,13 @@ __all__ = [
 #     * dimension table prefix
 #     * schema
 
-class StarBrowser(AggregationBrowser):
-    """docstring for StarBrowser"""
+class SnowflakeBrowser(AggregationBrowser):
+    """docstring for SnowflakeBrowser"""
 
-    def __init__(self, cube, connectable=None, locale=None, metadata=None, 
+    def __init__(self, cube, connectable=None, locale=None, metadata=None,
                  debug=False, **options):
-        """StarBrowser is a SQL-based AggregationBrowser implementation that 
-        can aggregate star and snowflake schemas without need of having 
+        """SnowflakeBrowser is a SQL-based AggregationBrowser implementation that
+        can aggregate star and snowflake schemas without need of having
         explicit view or physical denormalized table.
 
         Attributes:
@@ -83,7 +82,7 @@ class StarBrowser(AggregationBrowser):
         * only one locale can be used for browsing at a time
         * locale is implemented as denormalized: one column for each language
         """
-        super(StarBrowser, self).__init__(cube)
+        super(SnowflakeBrowser, self).__init__(cube)
 
         if cube == None:
             raise ArgumentError("Cube for browser should not be None.")
@@ -799,6 +798,61 @@ class QueryContext(object):
 
         return columns
 
+class AggregatedCubeBrowser(AggregationBrowser):
+    """docstring for SnowflakeBrowser"""
+
+    def __init__(self, cube, connectable=None, locale=None, metadata=None,
+                 debug=False, **options):
+        """AggregatedCubeBrowser is a SQL-based AggregationBrowser
+        implementation that uses pre-aggregated table.
+
+        Attributes:
+
+        * `cube` - browsed cube
+        * `connectable` - SQLAlchemy connectable object (engine or connection)
+        * `locale` - locale used for browsing
+        * `metadata` - SQLAlchemy MetaData object
+        * `debug` - output SQL to the logger at INFO level
+        * `options` - passed to the mapper and context (see their respective
+          documentation)
+
+        """
+        super(AggregatedCubeBrowser, self).__init__(cube)
+
+        if cube == None:
+            raise ArgumentError("Cube for browser should not be None.")
+
+        self.logger = get_logger()
+
+        self.cube = cube
+        self.locale = locale or cube.model.locale
+        self.debug = debug
+
+        if connectable is not None:
+            self.connectable = connectable
+            self.metadata = metadata or sqlalchemy.MetaData(bind=self.connectable)
+
+        # Mapper is responsible for finding corresponding physical columns to
+        # dimension attributes and fact measures. It also provides information
+        # about relevant joins to be able to retrieve certain attributes.
+
+        if options.get("use_denormalization"):
+            mapper_class = DenormalizedMapper
+        else:
+            mapper_class = SnowflakeMapper
+
+        self.logger.debug("using mapper %s for cube '%s' (locale: %s)" % \
+                            (str(mapper_class.__name__), cube.name, locale))
+
+        self.mapper = mapper_class(cube, locale=self.locale, **options)
+        self.logger.debug("mapper schema: %s" % self.mapper.schema)
+
+        # QueryContext is creating SQL statements (using SQLAlchemy). It
+        # also caches information about tables retrieved from metadata.
+
+        self.context = QueryContext(self.cube, self.mapper,
+                                      metadata=self.metadata)
+
 def paginated_statement(statement, page, page_size):
     """Returns paginated statement if page is provided, otherwise returns
     the same statement."""
@@ -907,344 +961,3 @@ class ResultIterator(object):
 
         return dict(zip(self.labels, row))
 
-####
-# Backend related functions
-###
-def ddl_for_model(url, model, fact_prefix=None, dimension_prefix=None, schema_type=None):
-    """Create a star schema DDL for a model.
-
-    Parameters:
-
-    * `url` - database url – no connection will be created, just used by
-       SQLAlchemy to determine appropriate engine backend
-    * `cube` - cube to be described
-    * `dimension_prefix` - prefix used for dimension tables
-    * `schema_type` - ``logical``, ``physical``, ``denormalized``
-
-    As model has no data storage type information, following simple rule is
-    used:
-
-    * fact ID is an integer
-    * all keys are strings
-    * all attributes are strings
-    * all measures are floats
-
-    .. warning::
-
-        Does not respect localized models yet.
-
-    """
-    raise NotImplementedError
-
-def create_workspace(model, **options):
-    """Create workspace for `model` with configuration in dictionary
-    `options`. This method is used by the slicer server.
-
-    The options are:
-
-    Required (one of the two, `engine` takes precedence):
-
-    * `url` - database URL in form of:
-      ``backend://user:password@host:port/database``
-    * `engine` - SQLAlchemy engine - either this or URL should be provided
-
-    Optional:
-
-    * `schema` - default schema, where all tables are located (if not
-      explicitly stated otherwise)
-    * `fact_prefix` - used by the snowflake mapper to find fact table for a
-      cube, when no explicit fact table name is specified
-    * `dimension_prefix` - used by snowflake mapper to find dimension tables
-      when no explicit mapping is specified
-    * `dimension_schema` – schema where dimension tables are stored, if
-      different than common schema.
-
-    Options for denormalized views:
-
-    * `use_denormalization` - browser will use dernormalized view instead of
-      snowflake
-    * `denormalized_view_prefix` - if denormalization is used, then this
-      prefix is added for cube name to find corresponding cube view
-    * `denormalized_view_schema` - schema wehere denormalized views are
-      located (use this if the views are in different schema than fact tables,
-      otherwise default schema is going to be used)
-    """
-    engine = options.get("engine")
-
-    if engine:
-        del options["engine"]
-    else:
-        try:
-            db_url = options["url"]
-        except KeyError:
-            raise ArgumentError("No URL or engine specified in options, "
-                                "provide at least one")
-        engine = sqlalchemy.create_engine(db_url)
-
-
-    workspace = SQLStarWorkspace(model, engine, **options)
-
-    return workspace
-
-class SQLStarWorkspace(object):
-    """Factory for browsers"""
-    def __init__(self, model, engine, **options):
-        """Create a workspace. For description of options see
-        `create_workspace()` """
-
-        super(SQLStarWorkspace, self).__init__()
-
-        self.logger = get_logger()
-
-        self.model = model
-        self.engine = engine
-        self.schema = options.get("schema")
-        self.metadata = sqlalchemy.MetaData(bind=self.engine,schema=self.schema)
-        self.options = options
-
-    def browser_for_cube(self, cube, locale=None):
-        """Creates, configures and returns a browser for a cube.
-
-        .. note::
-
-            Use `workspace.browser()` instead.
-        """
-
-        # TODO(Stiivi): make sure that we are leaking connections here
-        self.logger.info("workspace.create_browser() is depreciated, use "
-                         ".browser() instead")
-
-        return self.browser(cube, locale)
-
-    def browser(self, cube, locale=None):
-        """Returns a browser for a cube."""
-        cube = self.model.cube(cube)
-        browser = StarBrowser(cube, self.engine, locale=locale,
-                              metadata=self.metadata,
-                              **self.options)
-        return browser
-
-    def _drop_table(self, table, schema, force=False):
-        """Drops `table` in `schema`. If table exists, exception is raised
-        unless `force` is ``True``"""
-
-        view_name = str(table)
-        preparer = self.engine.dialect.preparer(self.engine.dialect)
-        full_name = preparer.format_table(table)
-
-        if table.exists() and not force:
-            raise WorkspaceError("View or table %s (schema: %s) already exists." % \
-                               (view_name, schema))
-
-        inspector = sqlalchemy.engine.reflection.Inspector.from_engine(self.engine)
-        view_names = inspector.get_view_names(schema=schema)
-
-        if view_name in view_names:
-            # Table reflects a view
-            drop_statement = "DROP VIEW %s" % full_name
-            self.engine.execute(drop_statement)
-        else:
-            # Table reflects a table
-            table.drop(checkfirst=False)
-
-    def create_denormalized_view(self, cube, view_name=None, materialize=False,
-                                 replace=False, create_index=False,
-                                 keys_only=False, schema=None):
-        """Creates a denormalized view named `view_name` of a `cube`. If
-        `view_name` is ``None`` then view name is constructed by pre-pending
-        value of `denormalized_view_prefix` from workspace options to the cube
-        name. If no prefix is specified in the options, then view name will be
-        equal to the cube name.
-
-        Options:
-
-        * `materialize` - whether the view is materialized (a table) or
-          regular view
-        * `replace` - if `True` then existing table/view will be replaced,
-          otherwise an exception is raised when trying to create view/table
-          with already existing name
-        * `create_index` - if `True` then index is created for each key
-          attribute. Can be used only on materialized view, otherwise raises
-          an exception
-        * `keys_only` - if ``True`` then only key attributes are used in the
-          view, all other detail attributes are ignored
-        * `schema` - target schema of the denormalized view, if not specified,
-          then `denormalized_view_schema` from options is used if specified,
-          otherwise default workspace schema is used (same schema as fact
-          table schema).
-        """
-
-        cube = self.model.cube(cube)
-
-        mapper = SnowflakeMapper(cube, cube.mappings, **self.options)
-        context = QueryContext(cube, mapper, metadata=self.metadata)
-
-        key_attributes = []
-        for dim in cube.dimensions:
-            key_attributes += dim.key_attributes()
-
-        if keys_only:
-            statement = context.denormalized_statement(attributes=key_attributes, expand_locales=True)
-        else:
-            statement = context.denormalized_statement(expand_locales=True)
-
-        schema = schema or self.options.get("denormalized_view_schema") or self.schema
-
-        dview_prefix = self.options.get("denormalized_view_prefix","")
-        view_name = view_name or dview_prefix + cube.name
-
-        if mapper.fact_name == view_name and schema == mapper.schema:
-            raise WorkspaceError("target denormalized view is the same as source fact table")
-
-        table = sqlalchemy.Table(view_name, self.metadata,
-                                 autoload=False, schema=schema)
-
-        preparer = self.engine.dialect.preparer(self.engine.dialect)
-        full_name = preparer.format_table(table)
-
-        if table.exists():
-            self._drop_table(table, schema, force=replace)
-
-        if materialize:
-            create_stat = "CREATE TABLE"
-        else:
-            create_stat = "CREATE OR REPLACE VIEW"
-
-        statement = "%s %s AS %s" % (create_stat, full_name, str(statement))
-        self.logger.info("creating denormalized view %s (materialized: %s)" \
-                                            % (full_name, materialize))
-        # print("SQL statement:\n%s" % statement)
-        self.engine.execute(statement)
-
-        if create_index:
-            if not materialize:
-                raise WorkspaceError("Index can be created only on a materialized view")
-
-            # self.metadata.reflect(schema = schema, only = [view_name] )
-            table = sqlalchemy.Table(view_name, self.metadata,
-                                     autoload=True, schema=schema)
-            self.engine.reflecttable(table)
-
-            for attribute in key_attributes:
-                label = attribute.ref()
-                self.logger.info("creating index for %s" % label)
-                column = table.c[label]
-                name = "idx_%s_%s" % (view_name, label)
-                index = sqlalchemy.schema.Index(name, column)
-                index.create(self.engine)
-
-        return statement
-
-    def validate_model(self):
-        """Validate physical representation of model. Returns a list of
-        dictionaries with keys: ``type``, ``issue``, ``object``.
-
-        Types might be: ``join`` or ``attribute``.
-
-        The ``join`` issues are:
-
-        * ``no_table`` - there is no table for join
-        * ``duplicity`` - either table or alias is specified more than once
-
-        The ``attribute`` issues are:
-
-        * ``no_table`` - there is no table for attribute
-        * ``no_column`` - there is no column for attribute
-        * ``duplicity`` - attribute is found more than once
-
-        """
-        issues = []
-
-        for cube in self.model.cubes:
-            browser = self.browser_for_cube(cube)
-            issues += browser.validate()
-
-        return issues
-
-    def compute(self, cube, dimensions):
-        pass
-
-
-    def create_aggregated_cube(self, cube, table_name=None, dimensions=None,
-                                schema=None, replace=False):
-        """Creates an aggregate table. If dimensions is `None` then all cube's
-        dimensions are considered.
-
-        Arguments:
-
-        * `prefix`: aggregated table prefix
-
-        """
-
-        schema = schema or self.options.get("aggregated_cubes_schema") or self.schema
-        prefix = self.options.get("aggregated_cubes_prefix","")
-        table_name = table_name or prefix + cube.name
-
-        cube = self.model.cube(cube)
-        dimensions = dimensions or cube.dimensions
-
-        # Collect keys that are going to be used for aggregations
-        keys = []
-        for dimension in dimensions:
-            keys += [level.key for level in dimension.hierarchy().levels]
-
-        mapper = SnowflakeMapper(cube, cube.mappings, **self.options)
-        context = QueryContext(cube, mapper, metadata=self.metadata)
-
-        if mapper.fact_name == table_name and schema == mapper.schema:
-            raise WorkspaceError("target is the same as source fact table")
-
-        drilldown = {}
-
-        for dim in dimensions:
-            level = dim.hierarchy().levels[-1]
-            drilldown[str(dim)] = level
-
-        cell = Cell(cube)
-        drilldown = coalesce_drilldown(cell, drilldown)
-
-        statement = context.aggregation_statement(cell,
-                                                  attributes=keys,
-                                                  drilldown=drilldown)
-
-
-        #
-        # Create table
-        #
-        table = sqlalchemy.Table(table_name, self.metadata,
-                                 autoload=False, schema=schema)
-
-        if table.exists():
-            self._drop_table(table, schema, force=replace)
-
-        for col in statement.columns:
-            new_col = sqlalchemy.Column(str(col), col.type)
-            table.append_column(new_col)
-
-        self.logger.info("creating aggregation table '%s'" % str(table))
-        self.metadata.create_all(tables=[table])
-
-        connection = self.engine.connect()
-
-        cuboids = hierarchical_cuboids(dimensions)
-        for cuboid in cuboids:
-            self.logger.info("aggregating cuboid %s" % (cuboid, ) )
-
-            dd = {}
-            keys = None
-            for dim, level in cuboid:
-                dd[str(dim)] = level
-                dim = cube.dimension(dim)
-                hier = dim.hierarchy()
-                levels = hier.levels_for_depth(hier.level_index(level)+1)
-                keys = [l.key for l in levels]
-
-            dd = coalesce_drilldown(cell, dd)
-
-            statement = context.aggregation_statement(cell,
-                                                      attributes=keys,
-                                                      drilldown=drilldown)
-            self.logger.info("inserting")
-            insert = InsertIntoAsSelect(table, statement,
-                                  columns=statement.columns)
-            connection.execute(str(insert))
