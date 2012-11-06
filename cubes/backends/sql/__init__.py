3988c8315f173f3a775007a91876b1eeb4870cd4
Stefan Urbanek <stefan@agentfarms.net>
2012-11-05 23:19:19 -0600

    fix imports

--- cubes/backends/sql/__init__.py
+++ cubes/backends/sql/__init__.py
@@ -1,14 +1,10 @@
 from denormalizer import *
-from browser import *
 from star import *
-from mapper import *
 from workspace import *
 
 __all__ = []
 
 __all__ += denormalizer.__all__
-__all__ += browser.__all__
 __all__ += star.__all__
-__all__ += mapper.__all__
 __all__ += workspace.__all__
 
