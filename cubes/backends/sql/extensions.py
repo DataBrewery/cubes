103276a5e4208cf81796c8ba4e178b9c8efcdf61
Stefan Urbanek <stefan@agentfarms.net>
2012-11-30 01:05:42 +0100

    added sql extensions: aggregate functions avg, stddev, variance

---
+++ cubes/backends/sql/extensions.py
@@ -0,0 +1,26 @@
+try:
+    import sqlalchemyd
+    import sqlalchemy.sql as sql
+    from sqlalchemy.sql.functions import ReturnTypeFromArgs
+
+
+except ImportError:
+    from cubes.common import MissingPackage
+    sqlalchemy = sql = MissingPackage("sqlalchemy", "SQL aggregation browser")
+    missing_error = MissingPackage("sqlalchemy", "SQL browser extensions")
+
+    class ReturnTypeFromArgs(object):
+        def __init__(*args, **kwargs):
+            # Just fail by trying to call missing package
+            missing_error()
+
+class avg(ReturnTypeFromArgs):
+    pass
+
+# Works with PostgreSQL
+class stddev(ReturnTypeFromArgs):
+    pass
+
+class variance(ReturnTypeFromArgs):
+    pass
+
