4c95f8cd93e601982a114c6bf03b014ae9d7f496
Brad Willard <bwillard@squarespace.com>
2013-04-09 17:06:30 -0400

    added support for week

---
+++ cubes/backends/mongo2/datesupport.py
@@ -0,0 +1,90 @@
+from dateutil.relativedelta import relativedelta
+from datetime import datetime, timedelta
+import pytz
+
+
+tz = pytz.timezone('America/New_York')
+tz_utc = pytz.timezone('UTC')
+
+DATE_PARTS = ['year', 'month', 'day']
+TIME_PARTS = ['hour', 'minute', 'second', 'microsecond']
+
+def eastern_date_as_utc(year, **kwargs):
+
+    dateparts = {'year': year, 'tzinfo': tz}
+    dateparts.update(kwargs)
+
+    date = datetime(**dateparts)
+
+    return date.astimezone(tz_utc)
+
+
+def get_date_for_week(year, week):
+    if week < 1:
+        raise ValueError('Week must be greater than 0')
+
+    dt = datetime(**{
+            'year': year,
+            'month': 1,
+            'day': 1
+        })
+
+    while dt.weekday() != 4:
+        dt += timedelta(1)
+
+    week -= 1
+    dt += timedelta(7 * week)
+
+    return dt
+
+
+def calc_week(dt):
+    
+    dt = get_next_weekdate(dt, direction='down')
+    year = dt.year
+
+    count = 0
+    while dt.year == year:
+        count += 1
+        dt -= timedelta(days=7)
+
+    return (year, count)
+
+
+def clear(dt, parts=TIME_PARTS):
+    replace_dict = {}
+
+    for p in parts:
+        replace_dict[p] = date_norm_map.get(p)
+
+    return dt.replace(**replace_dict)
+
+
+def get_next_weekdate(dt, direction='up'):
+    dr = clear(dt)
+    while dr.weekday() != 4:
+        if direction in set(['up', 'asc', '1', 1]):
+            dr += timedelta(1)
+        else:
+            dr -= timedelta(1)
+
+    return dr
+
+
+datepart_functions = {
+    'year': lambda x:x.year,
+    'month': lambda x:x.month,
+    'week': calc_week,
+    'day': lambda x:x.day,
+    'hour': lambda x:x.hour,
+}
+
+
+date_norm_map = {
+    'month': 1,
+    'day': 1,
+    'hour': 0,
+    'minute': 0,
+    'second': 0,
+    'microsecond': 0,
+}
