560d1bf2b300af1b9d7dfbbb4b9b5e528ff11ce2
Brad Willard <bwillard@squarespace.com>
2013-05-13 16:28:26 -0400

    Added timezone as a server level config

--- cubes/server/utils.py
+++ cubes/server/utils.py
@@ -1,3 +1,17 @@
+import pytz
+from time import gmtime, strftime
+from datetime import datetime, timedelta
+
+tz_utc = pytz.timezone('UTC')
+default_tz = pytz.timezone(strftime("%Z", gmtime()))
+
+
+def now(tzinfo=default_tz):
+    n = datetime.utcnow()
+    n = tz_utc.localize(n)
+    return n.astimezone(tzinfo)
+
+
 # Soft dependency on Werkzeug
 try:
     from werkzeug.local import Local, LocalManager
