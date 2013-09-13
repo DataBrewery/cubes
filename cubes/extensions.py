cd88e0d278e8195a05586b845fec64e5c6e03dca
Stefan Urbanek <stefan@agentfarms.net>
2013-09-13 11:00:46 +0200

    improved namespace - load default module when an object is missing

--- cubes/extensions.py
+++ cubes/extensions.py
@@ -1,27 +1,67 @@
 from .common import decamelize, to_identifier
 from collections import defaultdict
 
-_namespaces = defaultdict(dict)
+_default_modules = {
+        "stores": {
+                "sql":"cubes.backends.sql.store",
+                "mongo":"cubes.backends.mongo",
+                "mongo2":"cubes.backends.mongo2"
+            },
+        "browsers": {
+                "snowflake":"cubes.backends.sql.star",
+                "snapshot": "cubes.backends.sql.star",
+            },
+        "model_providers": {
+            }
+}
+
+class Namespace(dict):
+    def __init__(self, name, objects=None, root_class=None, suffix=None):
+        self.name = name
+        self.root_class = root_class
+        self.suffix = suffix
+
+        if objects:
+            self.update(objects)
+
+    def discover_objects(self):
+        if self.root_class:
+            objects = collect_subclasses(self.root_class, self.suffix)
+            self.update(objects)
+
+    def __getattr__(self, value):
+        return self.__getitem__(value)
+
+    def __getitem__(self, value):
+        try:
+            return super(Namespace, self).__getitem__(value)
+        except KeyError:
+            # Lazily load module that might contain the object
+            modules = _default_modules.get(self.name)
+            if modules and value in modules:
+                _load_module(modules[value])
+                self.discover_objects()
+
+            # Retry after loading
+            return super(Namespace, self).__getitem__(value)
+
+_namespaces = {}
 
 def get_namespace(name):
     """Gets a namespace `name` dictionary."""
 
-    return _namespaces[name]
+    return _namespaces.get(name)
 
 def initialize_namespace(name, objects=None, root_class=None, suffix=None):
     """Initializes the namespace `name` with `objects` dictionary and
     subclasses of `root_class` where the class name is decamelized, changet do
     an identifier and with `suffix` removed."""
 
-    if root_class:
-        base = collect_subclasses(root_class, suffix)
-    else:
-        base = {}
+    ns = Namespace(name, objects, root_class, suffix)
+    ns.discover_objects()
+    _namespaces[name] = ns
 
-    if objects:
-        base.update(objects)
-    _namespaces[name] = base
-    return base
+    return ns
 
 def collect_subclasses(parent, suffix=None):
     """Collect all subclasses of `parent` and return a dictionary where keys
@@ -66,3 +106,10 @@ def subclass_iterator(cls, _seen=None):
             for sub in subclass_iterator(sub, _seen):
                 yield sub
 
+def _load_module(modulepath):
+    mod = __import__(modulepath)
+    path = []
+    for token in modulepath.split(".")[1:]:
+       path.append(token)
+       mod = getattr(mod, token)
+    return mod
