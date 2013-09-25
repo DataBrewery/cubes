7983c17411c473aaa1395d47bd1ea18b17c09d58
Stefan Urbanek <stefan@agentfarms.net>
2013-09-25 20:22:03 +0200

    Will you join me? From left or right, it does not matter!
    Implemented new joining algorithm that can handle three join methods
    inner join ("match"/default), left outer join ("master") and right
    outer join ("detail").

--- tests/backends/sql/outer_joins.py
+++ tests/backends/sql/outer_joins.py
@@ -1,7 +1,13 @@
+# -*- coding=utf -*-
 import unittest
 from sqlalchemy import create_engine, MetaData, Table, Integer, String, Column
 from cubes import *
 from ...common import CubesTestCaseBase
+
+from json import dumps
+
+def printable(obj):
+    return dumps(obj, indent=4)
 
 class OuterJoinsTestCase(CubesTestCaseBase):
     def setUp(self):
@@ -24,18 +30,21 @@ class OuterJoinsTestCase(CubesTestCaseBase):
         metadata.create_all()
 
         data = [
-                    (1, 20130901,  10),
-                    (2, 20130902,  20),
-                    (3, 20130903,  40),
-                    (4, 20130910,  80),
-                    (5, 20130915, 160),
+                    (1, 20130901,   10),
+                    (2, 20130902,   20),
+                    (3, 20130903,   40),
+                    (4, 20130910,   80),
+                    (5, 20130915,  160),
+                    (6, 20131001,  320),
+                    (7, 20131002,  640),
+                    (8, 20131004, 1280),
                 ]
 
         for row in data:
             insert = self.facts.insert().values(data)
             engine.execute(insert)
 
-        for day in range(1, 30):
+        for day in range(1, 31):
             row = (20130900+day, 2013, 9, day)
             insert = self.dim_date.insert().values(row)
             engine.execute(insert)
@@ -53,30 +62,50 @@ class OuterJoinsTestCase(CubesTestCaseBase):
         self.workspace.logger.setLevel("DEBUG")
         self.workspace.logger.info("=== test setup")
 
+        self.logger = self.workspace.logger
+        self.day_drilldown = [("date", "default", "day")]
+        self.month_drilldown = [("date", "default", "month")]
+        self.year_drilldown = [("date", "default", "year")]
+
     def test_empty(self):
         browser = self.workspace.browser("facts")
         result = browser.aggregate()
 
-        self.assertEqual(1550, result.summary["amount_sum"])
+        self.assertEqual(20400, result.summary["amount_sum"])
 
-    def test_cell_count(self):
-        drilldown = [("date", "default", "day")]
+    def test_cell_count_match(self):
 
         browser = self.workspace.browser("facts")
-        result = browser.aggregate(drilldown=drilldown)
-        self.assertEqual(5, result.total_cell_count)
+        result = browser.aggregate(drilldown=self.day_drilldown)
+        self.assertEqual(5, len(list(result.cells)))
 
+        result = browser.aggregate(drilldown=self.month_drilldown)
+        cells = list(result.cells)
+        self.assertEqual(1, len(cells))
+
+    def test_cell_count_master(self):
         browser = self.workspace.browser("facts_master")
-        result = browser.aggregate(drilldown=drilldown)
-        self.assertEqual(5, result.total_cell_count)
 
+        # All results should be +1 compared to "match" – all unknown dates
+
+        result = browser.aggregate(drilldown=self.day_drilldown)
+        self.assertEqual(20400, result.summary["amount_sum"])
+        cells = list(result.cells)
+        self.assertEqual(6, len(cells))
+
+        result = browser.aggregate(drilldown=self.month_drilldown)
+        cells = list(result.cells)
+        self.assertEqual(2, len(cells))
+
+    def test_cell_count_detail(self):
         browser = self.workspace.browser("facts_detail")
-        result = browser.aggregate(drilldown=drilldown)
-        self.assertEqual(5, result.total_cell_count)
 
-    def test_drilldown(self):
-        browser = self.workspace.browser("facts_master")
-        result = browser.aggregate(drilldown=[("date", "default", "day")])
+        result = browser.aggregate(drilldown=self.day_drilldown)
+        self.assertEqual(20400, result.summary["amount_sum"])
+        cells = list(result.cells)
+        self.assertEqual(30, len(cells))
 
-        self.assertEqual(5, result.total_cell_count)
+        result = browser.aggregate(drilldown=self.month_drilldown)
+        cells = list(result.cells)
+        self.assertEqual(2, len(cells))
 
