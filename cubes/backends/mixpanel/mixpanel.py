5162668cd76b40317d3ff44e097ea8aac8735158
Stefan Urbanek <stefan@agentfarms.net>
2013-10-04 13:34:05 +0200

    Preliminary implementation of facts(); New "data" API endpoint; moved error handling into mixpanel.py

--- cubes/backends/mixpanel/mixpanel.py
+++ cubes/backends/mixpanel/mixpanel.py
@@ -12,16 +12,20 @@ try:
 except ImportError:
     import simplejson as json
 
+class MixpanelError(Exception):
+    pass
+
 class Mixpanel(object):
 
     ENDPOINT = 'http://mixpanel.com/api'
+    DATA_ENDPOINT = 'http://data.mixpanel.com/api'
     VERSION = '2.0'
 
     def __init__(self, api_key, api_secret):
         self.api_key = api_key
         self.api_secret = api_secret
 
-    def request(self, methods, params, format='json'):
+    def request(self, methods, params, format='json', is_data=False):
         """
             methods - List of methods to be joined, e.g. ['events', 'properties', 'values']
                       will give us http://mixpanel.com/api/2.0/events/properties/values/
@@ -33,22 +37,31 @@ class Mixpanel(object):
         if 'sig' in params: del params['sig']
         params['sig'] = self.hash_args(params)
 
-        request_url = '/'.join([self.ENDPOINT, str(self.VERSION)] + methods) + '/?' + self.unicode_urlencode(params)
+        endpoint = self.DATA_ENDPOINT if is_data else self.ENDPOINT
+        request_url = '/'.join([endpoint, str(self.VERSION)] + methods) + '/?' + self.unicode_urlencode(params)
 
-        request = urllib.urlopen(request_url)
-        data = request.read()
+        response = urllib.urlopen(request_url)
 
-        return json.loads(data)
+        # Standard response or error response is a JSON. Data response is one
+        # JSON per line
+        if not is_data or response.getcode() != 200:
+            result = json.loads(response.read())
+            if "error" in result:
+                raise MixpanelError(result["error"])
+            else:
+                return result
+        else:
+            return _MixpanelDataIterator(response)
 
     def unicode_urlencode(self, params):
         """
-            Convert lists to JSON encoded strings, and correctly handle any 
+            Convert lists to JSON encoded strings, and correctly handle any
             unicode URL parameters.
         """
         if isinstance(params, dict):
             params = params.items()
         for i, param in enumerate(params):
-            if isinstance(param[1], list): 
+            if isinstance(param[1], list):
                 params[i] = (param[0], json.dumps(param[1]),)
 
         return urllib.urlencode(
@@ -57,7 +70,7 @@ class Mixpanel(object):
 
     def hash_args(self, args, secret=None):
         """
-            Hashes arguments by joining key=value pairs, appending a secret, and 
+            Hashes arguments by joining key=value pairs, appending a secret, and
             then taking the MD5 hex digest.
         """
         for a in args:
@@ -84,6 +97,27 @@ class Mixpanel(object):
         elif self.api_secret:
             hash.update(self.api_secret)
         return hash.hexdigest()
+
+class _MixpanelDataIterator(object):
+    def __init__(self, data):
+        self.line_iterator = iter(data)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        # From Mixpanel Documentation:
+        #
+        # One event per line, sorted by increasing timestamp. Each line is a
+        # JSON dict, but the file itself is not valid JSON because there is no
+        # enclosing object. Timestamps are in project time but expressed as
+        # Unix time codes (but from_date and to_date are still interpreted in
+        # project time).
+        #
+        # Source: https://mixpanel.com/docs/api-documentation/exporting-raw-data-you-inserted-into-mixpanel
+        #
+        line = next(self.line_iterator)
+        return json.loads(line)
 
 if __name__ == '__main__':
     api = Mixpanel(
