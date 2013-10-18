00ed85c0ac6f3e9b3f0b29fc118120280fbca1dc
Stefan Urbanek <stefan@agentfarms.net>
2013-10-18 18:07:41 +0200

    Started new 'time' module

--- tests/time.py
+++ tests/time.py
@@ -1,9 +1,7 @@
-# -*- coding=utf -*-
 from common import CubesTestCaseBase
 from cubes.errors import *
 from cubes.time import *
 from cubes import time_hierarchy_elements
-from cubes.calendar import *
 from datetime import datetime
 
 
@@ -12,7 +10,6 @@ class DateTimeTestCase(CubesTestCaseBase):
         super(DateTimeTestCase,self).setUp()
 
         self.workspace = self.create_workspace(model="datetime.json")
-        self.cal = Calendar()
 
     def test_empty(self):
         dim = self.workspace.dimension("default_date")
@@ -23,111 +20,26 @@ class DateTimeTestCase(CubesTestCaseBase):
     def test_implicit_roles(self):
         dim = self.workspace.dimension("default_date")
 
-        elements = calendar_hierarchy_units(dim.hierarchy("ymd"))
+        elements = time_hierarchy_elements(dim.hierarchy("ymd"))
         self.assertSequenceEqual(["year", "month", "day"], elements)
 
     def test_explicit_roles(self):
         dim = self.workspace.dimension("explicit_date")
 
-        elements = calendar_hierarchy_units(dim.hierarchy("ymd"))
+        elements = time_hierarchy_elements(dim.hierarchy("ymd"))
         self.assertSequenceEqual(["year", "month", "day"], elements)
 
     def test_no_roles(self):
         dim = self.workspace.dimension("invalid_date")
 
         with self.assertRaises(ArgumentError):
-            calendar_hierarchy_units(dim.hierarchy("ymd"))
+            time_hierarchy_elements(dim.hierarchy("ymd"))
 
     def test_time_path(self):
         date = datetime(2012, 12, 24)
 
-        self.assertEqual([], self.cal.path(date, []))
-        self.assertEqual([2012], self.cal.path(date, ["year"]))
-        self.assertEqual([12, 24], self.cal.path(date, ["month", "day"]))
-        self.assertEqual([2012, 4], self.cal.path(date, ["year", "quarter"]))
+        self.assertEqual([], time_path(date, []))
+        self.assertEqual([2012], time_path(date, ["year"]))
+        self.assertEqual([12, 24], time_path(date, ["month", "day"]))
+        self.assertEqual([2012, 4], time_path(date, ["year", "quarter"]))
 
-    def test_path_weekday(self):
-        # This is monday:
-        date = datetime(2013, 10, 21)
-        self.assertEqual([0], self.cal.path(date, ["weekday"]))
-
-        # Week start: Sunday
-        self.cal.first_weekday = 6
-        self.assertEqual([1], self.cal.path(date, ["weekday"]))
-
-        # Week start: Saturday
-        self.cal.first_weekday = 5
-        self.assertEqual([2], self.cal.path(date, ["weekday"]))
-
-    # Reference for the named relative test
-    #                              2012
-    # 
-    #     Január            Február           Marec             Apríl
-    # po     2  9 16 23 30     6 13 20 27        5*12 19 26        2  9 16 23 30
-    # ut     3 10 17 24 31     7 14 21 28        6 13 20 27        3 10 17 24
-    # st     4 11 18 25     1  8 15 22 29        7 14 21 28        4 11 18 25
-    # št     5 12 19 26     2  9 16 23       *1  8 15 22 29        5 12 19 26
-    # pi     6 13 20 27     3 10 17 24        2  9 16 23 30        6 13 20 27
-    # so     7 14 21 28     4 11 18 25        3 10 17 24 31        7 14 21 28
-    # ne  1  8 15 22 29     5 12 19 26        4 11 18 25        1  8 15 22 29
-
-    def test_named_relative(self):
-        date = datetime(2012, 3, 1)
-
-        units = ["year", "month", "day"]
-        path = self.cal.named_relative_path("tomorrow", units, date)
-        self.assertEqual([2012, 3, 2], path)
-
-        path = self.cal.named_relative_path("yesterday", units, date)
-        self.assertEqual([2012, 2, 29], path)
-
-        path = self.cal.named_relative_path("weekago", units, date)
-        self.assertEqual([2012, 2, 23], path)
-
-        path = self.cal.named_relative_path("3weeksago", units, date)
-        self.assertEqual([2012, 2, 9], path)
-
-        date = datetime(2012, 3, 12)
-
-        path = self.cal.named_relative_path("monthago", units, date)
-        self.assertEqual([2012, 2, 12], path)
-
-        path = self.cal.named_relative_path("12monthsago", units, date)
-        self.assertEqual([2011, 3, 12], path)
-
-        path = self.cal.named_relative_path("monthforward", units, date)
-        self.assertEqual([2012, 4, 12], path)
-
-        path = self.cal.named_relative_path("12monthsforward", units, date)
-        self.assertEqual([2013, 3, 12], path)
-
-    def test_named_relative_truncated(self):
-        date = datetime(2012, 3, 1, 10, 30)
-
-        units = ["year", "month", "day", "hour"]
-
-        path = self.cal.named_relative_path("lastweek", units, date)
-        self.assertEqual([2012, 2, 20, 0], path)
-
-        path = self.cal.named_relative_path("last3weeks", units, date)
-        self.assertEqual([2012, 2, 6, 0], path)
-
-        date = datetime(2012, 3, 12)
-
-        path = self.cal.named_relative_path("lastmonth", units, date)
-        self.assertEqual([2012, 2, 1, 0], path)
-
-        path = self.cal.named_relative_path("last12months", units, date)
-        self.assertEqual([2011, 3, 1, 0], path)
-
-        path = self.cal.named_relative_path("nextmonth", units, date)
-        self.assertEqual([2012, 4, 1, 0], path)
-
-        path = self.cal.named_relative_path("next12months", units, date)
-        self.assertEqual([2013, 3, 1,0 ], path)
-
-        path = self.cal.named_relative_path("lastquarter", units, date)
-        self.assertEqual([2012, 1, 1, 0], path)
-
-        path = self.cal.named_relative_path("lastyear", units, date)
-        self.assertEqual([2011, 1, 1,0 ], path)
