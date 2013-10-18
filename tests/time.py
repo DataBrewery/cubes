776ec5de31cc866879eeb77476f83c1dec072b09
Stefan Urbanek <stefan@agentfarms.net>
2013-10-18 18:07:41 +0200

    Started new 'time' module

--- tests/time.py
+++ tests/time.py
@@ -1,6 +1,11 @@
 from common import CubesTestCaseBase
 from cubes.errors import *
+<<<<<<< HEAD
 from cubes.calendar import *
+=======
+from cubes.time import *
+from cubes import time_hierarchy_elements
+>>>>>>> Started new 'time' module
 from datetime import datetime
 
 
@@ -9,7 +14,10 @@ class DateTimeTestCase(CubesTestCaseBase):
         super(DateTimeTestCase,self).setUp()
 
         self.workspace = self.create_workspace(model="datetime.json")
+<<<<<<< HEAD
         self.cal = Calendar()
+=======
+>>>>>>> Started new 'time' module
 
     def test_empty(self):
         dim = self.workspace.dimension("default_date")
@@ -20,24 +28,37 @@ class DateTimeTestCase(CubesTestCaseBase):
     def test_implicit_roles(self):
         dim = self.workspace.dimension("default_date")
 
+<<<<<<< HEAD
         elements = calendar_hierarchy_units(dim.hierarchy("ymd"))
+=======
+        elements = time_hierarchy_elements(dim.hierarchy("ymd"))
+>>>>>>> Started new 'time' module
         self.assertSequenceEqual(["year", "month", "day"], elements)
 
     def test_explicit_roles(self):
         dim = self.workspace.dimension("explicit_date")
 
+<<<<<<< HEAD
         elements = calendar_hierarchy_units(dim.hierarchy("ymd"))
+=======
+        elements = time_hierarchy_elements(dim.hierarchy("ymd"))
+>>>>>>> Started new 'time' module
         self.assertSequenceEqual(["year", "month", "day"], elements)
 
     def test_no_roles(self):
         dim = self.workspace.dimension("invalid_date")
 
         with self.assertRaises(ArgumentError):
+<<<<<<< HEAD
             calendar_hierarchy_units(dim.hierarchy("ymd"))
+=======
+            time_hierarchy_elements(dim.hierarchy("ymd"))
+>>>>>>> Started new 'time' module
 
     def test_time_path(self):
         date = datetime(2012, 12, 24)
 
+<<<<<<< HEAD
         self.assertEqual([], self.cal.path(date, []))
         self.assertEqual([2012], self.cal.path(date, ["year"]))
         self.assertEqual([12, 24], self.cal.path(date, ["month", "day"]))
@@ -60,3 +81,10 @@ class DateTimeTestCase(CubesTestCaseBase):
         date = datetime(2012, 1, 10)
         return
         self.assertEqual(datetime(2012, 1, 10), 0, "year")
+=======
+        self.assertEqual([], time_path(date, []))
+        self.assertEqual([2012], time_path(date, ["year"]))
+        self.assertEqual([12, 24], time_path(date, ["month", "day"]))
+        self.assertEqual([2012, 4], time_path(date, ["year", "quarter"]))
+
+>>>>>>> Started new 'time' module
