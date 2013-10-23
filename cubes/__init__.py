ad631e05314a9c5fcb9bed5dfa6ea8f0c0ea2430
Stefan Urbanek <stefan@agentfarms.net>
2013-10-23 18:36:00 +0200

    Added preliminary implementation of Authorizer/SimpleAuthorizer.

--- cubes/__init__.py
+++ cubes/__init__.py
@@ -2,19 +2,29 @@
 
 __version__ = "0.11.2"
 
-from .common import *
-from .browser import *
-from .model import *
-from .workspace import *
-from .errors import *
-from .server import *
-from .formatter import *
-from .computation import *
-from .mapper import *
-from .providers import *
-from .calendar import *
+from common import *
+from browser import *
+from model import *
+from workspace import *
+from errors import *
+from server import *
+from formatter import *
+from computation import *
+from mapper import *
+from providers import *
+from auth import *
 
 import backends
 import statutils
 
 
+__all__ += common.__all__
+__all__ += browser.__all__
+__all__ += model.__all__
+__all__ += workspace.__all__
+__all__ += server.__all__
+__all__ += formatter.__all__
+__all__ += computation.__all__
+__all__ += mapper.__all__
+__all__ += providers.__all__
+__all__ += auth.__all__
