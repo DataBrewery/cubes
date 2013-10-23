2a8a85f03e5e3d308384b7911316ec966c451abb
Stefan Urbanek <stefan@agentfarms.net>
2013-10-23 18:36:00 +0200

    Added preliminary implementation of Authorizer/SimpleAuthorizer.

--- cubes/auth.py
+++ cubes/auth.py
@@ -2,11 +2,10 @@
 
 import os.path
 import json
-from collections import namedtuple
+
 from .extensions import get_namespace, initialize_namespace
 from .browser import Cell
 from .errors import *
-from .common import read_json_file, sorted_dependencies
 
 __all__ = (
     "create_authorizer",
@@ -28,7 +27,7 @@ def create_authorizer(name, **options):
     ns = get_namespace("authorizers")
     if not ns:
         ns = initialize_namespace("authorizers", root_class=Authorizer,
-                                  suffix="_authorizer",
+                                  suffix="_store",
                                   option_checking=True)
 
     try:
@@ -53,121 +52,72 @@ class Authorizer(object):
         raise NotImplementedError
 
 
-class _SimpleAccessRight(object):
-    def __init__(self, roles, allow_cubes, deny_cubes, cube_restrictions):
-        self.roles = roles or []
-        self.allow_cubes = set(allow_cubes) if allow_cubes else set()
-        self.deny_cubes = set(deny_cubes) if deny_cubes else set()
-        self.cube_restrictions = cube_restrictions or {}
-
-    def merge(self, other):
-        """Merge `right` with the receiver:
-
-        * `allow_cubes` are merged (union)
-        * `deny_cubes` are merged (union)
-        * `cube_restrictions` from `other` with same cube replace restrictions
-          from the receiver"""
-
-        self.allow_cubes |= other.allow_cubes
-        self.deny_cubes |= other.deny_cubes
-
-        for cube, restrictions in other.cube_restrictions:
-            if not cube in self.cube_restrictions:
-                self.cube_restrictions = list(other.cube_restrictions)
-            else:
-                mine = self.cube_restrictions.get(cube)
-                mine += restritions
-
-
 class SimpleAuthorizer(Authorizer):
     __options__ = [
         {
-            "name": "rights_file",
-            "description": "JSON file with access rights",
-            "type": "string"
-        },
-        {
-            "name": "roles_file",
-            "description": "JSON file with access right roles",
+            "name": "tokens_file",
+            "description": "JSON file with authorization tokens and their "
+                           "respective configuration (cubes, cells, ...).",
             "type": "string"
         },
 
     ]
 
-    def __init__(self, rights_file=None, roles_file=None, roles=None,
-                 rights=None, **options):
-        """Creates a simple JSON-file based authorizer. Reads data from
-        `rights_file` and `roles_file` and merge them with `roles` and
-        `rights` dictionaries respectively."""
+    def __init__(self, tokens_file=None, tokens=None):
+        super(SimpleAuthorizer, self).__init__(self)
 
-        super(SimpleAuthorizer, self).__init__()
+        if tokens_file or tokens:
+            raise ArgumentError("Both tokens_file and tokens provided, "
+                                "use only one.")
 
-        roles = roles or {}
-        rights = rights or {}
+        if tokens_file:
+            if not os.path.exists(tokens_file):
+                raise ConfigurationError("Can not find tokens file '%s'"
+                                         % tokens_file)
 
-        if roles_file:
-            content = read_json_file(roles_file, "access roles")
-            roles.update(content)
+            try:
+                f = open(tokens_file)
+            except IOError:
+                raise ConfigurationError("Can not open tokens file '%s'"
+                                         % tokens_file)
 
-        if rights_file:
-            content = read_json_file(rights_file, "access rights")
-            rights.update(content)
+            try:
+                tokens = json.load(f)
+            except ValueError as e:
+                raise SyntaxError("Syntax error in tokens file %s: %s"
+                                  % (tokens_file, str(e)))
+            finally:
+                f.close()
 
-        self.roles = {}
-        self.rights = {}
+        elif not tokens:
+            raise ArgumentError("Neither tokens nor tokens_file provided")
 
-        # Process the roles
-        for key, info in roles.items():
-            role = _SimpleAccessRight(roles=info.get("roles"),
-                                      allow_cubes=info.get("allow_cubes"),
-                                      deny_cubes=info.get("deny_cubes"),
-                                      cube_restrictions=info.get("cube_restrictions"))
-            self.roles[key] = role
+        self.tokens = {}
 
-        deps = dict((name, role.roles) for name, role in self.roles.items())
-        order = sorted_dependencies(deps)
+        for token, info in tokens:
+            self._add_token(token, info)
 
-        for name in order:
-            role = self.roles[name]
-            for parent_name in role.roles:
-                parent = self.roles[parent_name]
-                role.merge(parent)
-
-        # Process rights
-        for key, info in rights.items():
-            right = _SimpleAccessRight(roles=info.get("roles"),
-                                       allow_cubes=info.get("allow_cubes"),
-                                       deny_cubes=info.get("deny_cubes"),
-                                       cube_restrictions=info.get("cube_restrictions"))
-            self.rights[key] = right
-
-            for role_name in right.roles:
-                role = self.roles[role_name]
-                right.merge(role)
+    def add_token(self, token, info):
+        """Prepare the token's info dictionary and add it to the
+        receipient."""
+        info = dict(info)
+        if "allow_cubes" not in info:
+            info["allowed_cubes"] = []
+        if "deny_cubes" not in info:
+            info["deny_cubes"] = []
 
     def authorize(self, token, cube, cell=None):
         try:
-            right = self.rights[token]
+            info = self.tokens[token]
         except KeyError:
-            raise NotAuthorized("Unknown access right '%s'" % token)
+            raise NotAuthorized("Unknown token")
 
-        cube_name = str(cube)
+        allow = info.get("allow_cubes", [])
+        deny = info.get("deny_cubes")
+        name = str(cube)
 
-        if (right.allow_cubes and cube_name not in right.allow_cubes) \
-                or (right.deny_cubes and cube_name in right.deny_cubes):
-            raise NotAuthorized("Unauthorized cube '%s' for '%s'"
-                                % (cube_name, token))
+        if (allow and name not in allow) \
+                or (deny and name in deny):
+            raise NotAuthorized("Unauthorized cube '%s'" % name)
 
-        cuts = right.cube_restrictions.get(cube_name)
-
-        if cuts:
-            cuts = [cut_from_dict(cut) for cut in cuts]
-            constraint_cell = Cell(cube, cuts)
-        else:
-            constraint_cell = Cell(cube)
-
-        if cell:
-            return cell + constraint_cell
-        else:
-            return constraint_cell
-
+        return cell
