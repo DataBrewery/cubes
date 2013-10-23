2901f8005e16f40f58a3a734f884b979c1162db0
Stefan Urbanek <stefan@agentfarms.net>
2013-10-23 23:01:10 +0200

    Added utilities: sorted_dependencies() and read_json_file()

--- tests/auth.py
+++ tests/auth.py
@@ -9,68 +9,7 @@ def printable(obj):
     return dumps(obj, indent=4)
 
 class AuthTestCase(CubesTestCaseBase):
-    def setUp(self):
-        self.sales_cube = Cube("sales")
-        self.churn_cube = Cube("churn")
-
     def test_empty(self):
         self.auth = SimpleAuthorizer()
         with self.assertRaises(NotAuthorized):
             self.auth.authorize("john", "sales")
-
-    def test_authorize(self):
-        rights = {
-            "john": {"allow_cubes": ["sales"]}
-        }
-        self.auth = SimpleAuthorizer(rights=rights)
-
-        self.assertFalse(self.auth.authorize("john", self.sales_cube))
-
-        with self.assertRaises(NotAuthorized):
-            self.auth.authorize("john", self.churn_cube)
-
-        with self.assertRaises(NotAuthorized):
-            self.auth.authorize("ivana", self.sales_cube)
-
-    def test_deny(self):
-        rights = {
-            "john": {"deny_cubes": ["sales"]}
-        }
-        self.auth = SimpleAuthorizer(rights=rights)
-
-        self.assertFalse(self.auth.authorize("john", self.churn_cube))
-
-        with self.assertRaises(NotAuthorized):
-            self.auth.authorize("john", self.sales_cube)
-
-        with self.assertRaises(NotAuthorized):
-            self.auth.authorize("ivana", self.churn_cube)
-
-    def test_role(self):
-        roles = {
-            "marketing": {"allow_cubes": ["sales"]}
-        }
-        rights = {
-            "john": {"roles": ["marketing"]}
-        }
-        self.auth = SimpleAuthorizer(rights=rights, roles=roles)
-
-        self.assertFalse(self.auth.authorize("john", self.sales_cube))
-
-        with self.assertRaises(NotAuthorized):
-            self.auth.authorize("john", self.churn_cube)
-
-    def test_role_inheritance(self):
-        roles = {
-            "top": {"allow_cubes": ["sales"]},
-            "marketing": {"roles": ["top"]}
-        }
-        rights = {
-            "john": {"roles": ["marketing"]}
-        }
-        self.auth = SimpleAuthorizer(rights=rights, roles=roles)
-
-        self.assertFalse(self.auth.authorize("john", self.sales_cube))
-
-        with self.assertRaises(NotAuthorized):
-            self.auth.authorize("john", self.churn_cube)
