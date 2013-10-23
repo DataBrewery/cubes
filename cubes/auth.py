fad16365fb604423a4a04d11cd4e3f06e2c49e9c
Stefan Urbanek <stefan@agentfarms.net>
2013-10-23 18:36:00 +0200

    Added preliminary implementation of Authorizer/SimpleAuthorizer.

--- cubes/auth.py
+++ cubes/auth.py
@@ -27,7 +27,7 @@ def create_authorizer(name, **options):
     ns = get_namespace("authorizers")
     if not ns:
         ns = initialize_namespace("authorizers", root_class=Authorizer,
-                                  suffix="_authorizer",
+                                  suffix="_store",
                                   option_checking=True)
 
     try:
