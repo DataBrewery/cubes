82d9368a1d41807cf410e342bda4f088369448fd
Stefan Urbanek <stefan@agentfarms.net>
2013-10-26 16:07:56 +0200

    Reorganized the server package

--- cubes/server/local.py
+++ cubes/server/local.py
@@ -8,10 +8,10 @@ from werkzeug.local import LocalProxy
 # Readability proxies
 
 def _get_workspace():
-    return current_app.cubes_workspace
+    return current_app.workspace
 
 def _get_logger():
-    return current_app.cubes_workspace.logger
+    return current_app.cubes_logger
 
 workspace = LocalProxy(_get_workspace)
 logger = LocalProxy(_get_logger)
