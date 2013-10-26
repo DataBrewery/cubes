b176a22af7d6edb4f3cb0efc19f498893548edee
Stefan Urbanek <stefan@agentfarms.net>
2013-10-26 18:59:32 +0200

    Since server is not imported by-default, just provide public objects

--- cubes/server/__init__.py
+++ cubes/server/__init__.py
@@ -1,18 +1,2 @@
-try:
-    from blueprint import slicer, API_VERSION
-    from base import run_server, create_server
-except ImportError:
-    from ..common import MissingPackage
-    _missing = MissingPackage("flask", "Slicer server")
-    slicer = _missing
-    run_server = _missing
-    create_server = _missing
-    API_VERSION = "unknown"
-
-
-__all__ = (
-    "slicer",
-    "run_server",
-    "create_server",
-    "API_VERSION"
-)
+from blueprint import slicer, API_VERSION
+from base import run_server, create_server
