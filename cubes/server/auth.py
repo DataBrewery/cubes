ac6d7dc8921de3bbeb6aa22a87d6fad673378eed
Stefan Urbanek <stefan@agentfarms.net>
2013-10-26 19:08:55 +0200

    Added basic server authentication functionality

--- cubes/server/auth.py
+++ cubes/server/auth.py
@@ -2,11 +2,6 @@
 from ..extensions import get_namespace, initialize_namespace
 from ..errors import *
 
-__all__ = (
-    "Authenticator",
-    "NotAuthenticated"
-)
-
 # IMPORTANT: This is provisional code. Might be changed or removed.
 #
 
@@ -20,7 +15,7 @@ def create_authenticator(name, **options):
     ns = get_namespace("slicer_authenticators")
     if not ns:
         ns = initialize_namespace("slicer_authenticators",
-                                  root_class=Authenticator,
+                                  root_class=SlicerAuthenticator,
                                   suffix="_authenticator",
                                   option_checking=True)
 
@@ -32,42 +27,30 @@ def create_authenticator(name, **options):
     return factory(**options)
 
 
-class Authenticator(object):
+class SlicerAuthenticator(object):
     def authenticate(self, request):
         raise NotImplementedError
 
 
-class AdminAdminAuthenticator(Authenticator):
+class AdminAdminAuthenticator(SlicerAuthenticator):
     """Simple HTTP Basic authenticator for testing purposes. User name and
     password have to be the same. User name is passed as the authenticated
     identity."""
+
     def authenticate(self, request):
         auth = request.authorization
-        if auth and auth.username == auth.password:
-            return auth.username
-        else:
-            raise NotAuthenticated
-
+        if auth:
+            if auth.username == auth.password:
+                return auth.username
+            else:
+                raise NotAuthenticated
         raise NotAuthenticated
 
 
-class PassParameterAuthenticator(Authenticator):
-    """Permissive authenticator that passes an URL parameter (default
-    ``api_key``) as idenity."""
-    def __init__(self, parameter=None, **options):
-        super(PassParameterAuthenticator, self).__init__(**options)
-        self.parameter_name = parameter or "api_key"
-
+class BasicHTTPAuthenticator(SlicerAuthenticator):
     def authenticate(self, request):
-        return request.args.get(self.parameter_name)
-
-
-class HTTPBasicProxyAuthenticator(Authenticator):
-    def authenticate(self, request):
-        """Permissive authenticator using HTTP Basic authentication that
-        assumes the server to be behind a proxy. Does not check for a
-        password, just passes the `username` as identity"""
         auth = request.authorization
+        print "=== AUTH RECEIVED: ", auth
         if auth:
             return auth.username
 
--- cubes/server/local.py
+++ cubes/server/local.py
@@ -8,10 +8,10 @@ from werkzeug.local import LocalProxy
 # Readability proxies
 
 def _get_workspace():
-    return current_app.workspace
+    return current_app.cubes_workspace
 
 def _get_logger():
-    return current_app.cubes_logger
+    return current_app.cubes_workspace.logger
 
 workspace = LocalProxy(_get_workspace)
 logger = LocalProxy(_get_logger)
