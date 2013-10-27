b2baca6651c09744e1f926fec92f2c9c681294ac
Stefan Urbanek <stefan@agentfarms.net>
2013-10-27 13:31:33 +0100

    Added built-in authenticators and better extension support; removed authorization_method (replaced by the permissive authenticators)

--- cubes/server/__init__.py
+++ cubes/server/__init__.py
@@ -1,2 +1,3 @@
-from blueprint import slicer, API_VERSION
-from base import run_server, create_server
+from .blueprint import slicer, API_VERSION
+from .base import run_server, create_server
+from .auth import Authenticator, NotAuthenticated
