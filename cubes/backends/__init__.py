a9eac5935bcb9c9fd29b4b150e89f83d7fdf9f56
Stefan Urbanek <stefan@agentfarms.net>
2013-11-07 15:45:13 +0100

    removed obsolete files

--- cubes/backends/__init__.py
+++ cubes/backends/__init__.py
@@ -1,13 +1,4 @@
-from mongo import *
 from mongo2 import *
 from sql import *
 from slicer import *
-import composite
 
-__all__ = [
-    "SQLDenormalizer",
-    "SQLBrowser",
-    "MongoSimpleCubeBrowser",
-    "MongoSimpleCubeBuilder",
-    "SlicerBrowser"
-]
--- cubes/backends/mongo/__init__.py
+++
@@ -1,8 +0,0 @@
-from base import *
-from browser import *
-from builder import *
-from workspace import *
-
-__all__ = []
-
-__all__ += workspace.__all__
