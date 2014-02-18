229e6dc2db1bd9bb9b7d6e60dbcbe242536822f9
Stefan Urbanek <stefan@agentfarms.net>
2014-02-18 13:55:11 +0100

    [backend-sql] Removed query debug

--- cubes/backends/sql/browser.py
+++ cubes/backends/sql/browser.py
@@ -26,31 +26,6 @@ __all__ = [
     "SnowflakeBrowser",
 ]
 
-#### DEBUG ### DEBUG ### DEBUG ### DEBUG ### DEBUG ### DEBUG ### DEBUG ###  
-
-from sqlalchemy import event
-from sqlalchemy.engine import Engine
-import time
-import logging
-
-
-@event.listens_for(Engine, "before_cursor_execute")
-def before_cursor_execute(conn, cursor, statement,
-                        parameters, context, executemany):
-    context._query_start_time = time.time()
-    logger = get_logger()
-    logger.debug(">>> Start Query: %s" % statement)
-
-@event.listens_for(Engine, "after_cursor_execute")
-def after_cursor_execute(conn, cursor, statement,
-                        parameters, context, executemany):
-    total = time.time() - context._query_start_time
-    logger = get_logger()
-    logger.debug("--- Query Complete!")
-    logger.debug("<<< Total Time: %f" % total)
-
-#### DEBUG ### DEBUG ### DEBUG ### DEBUG ### DEBUG ### DEBUG ### DEBUG ###  
-
 
 class SnowflakeBrowser(AggregationBrowser):
     __options__ = [
