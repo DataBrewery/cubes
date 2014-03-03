7db2358fcc4ff7e0652b441d5181cc31ad0c8ad6
Stefan Urbanek <stefan@agentfarms.net>
2014-03-03 21:44:43 +0100

    Moved validation to metadata

--- cubes/metadata.py
+++ cubes/metadata.py
@@ -13,6 +13,8 @@ the future.
 
 """
 
+import jsonschema
+import pkgutil
 import urlparse
 import urllib2
 import shutil
@@ -20,7 +22,7 @@ import json
 import os
 import re
 
-from collections import OrderedDict
+from collections import OrderedDict, namedtuple
 from .errors import *
 
 __all__ = (
@@ -33,6 +35,8 @@ __all__ = (
     "expand_dimension_metadata",
     "expand_level_metadata",
     "expand_attribute_metadata",
+
+    "validate_model",
 )
 
 # TODO: add the following:
@@ -368,3 +372,104 @@ def expand_attribute_metadata(metadata):
 
     return metadata
 
+
+ValidationError = namedtuple("ValidationError",
+                            ["severity", "scope", "object", "property", "message"])
+
+
+def validate_model(metadata):
+    """Validate model metadata."""
+
+    validator = ModelMetadataValidator(metadata)
+    return validator.validate()
+
+class ModelMetadataValidator(object):
+    def __init__(self, metadata):
+        self.metadata = metadata
+
+        data = pkgutil.get_data("cubes", "schemas/model.json")
+        self.model_schema = json.loads(data)
+
+        data = pkgutil.get_data("cubes", "schemas/cube.json")
+        self.cube_schema = json.loads(data)
+
+        data = pkgutil.get_data("cubes", "schemas/dimension.json")
+        self.dimension_schema = json.loads(data)
+
+    def validate(self):
+        errors = []
+
+        errors += self.validate_model()
+
+        if "cubes" in self.metadata:
+            for cube in self.metadata["cubes"]:
+                errors += self.validate_cube(cube)
+
+        if "dimensions" in self.metadata:
+            for dim in self.metadata["dimensions"]:
+                errors += self.validate_dimension(dim)
+
+        return errors
+
+    def _collect_errors(self, scope, obj, validator, metadata):
+        errors = []
+
+        for error in validator.iter_errors(metadata):
+            if error.path:
+                path = [str(item) for item in error.path]
+                ref = ".".join(path)
+            else:
+                ref = None
+
+            verror = ValidationError("error", scope, obj, ref, error.message)
+            errors.append(verror)
+
+        return errors
+
+    def validate_model(self):
+        validator = jsonschema.Draft4Validator(self.model_schema)
+        errors = self._collect_errors("model", None, validator, self.metadata)
+
+        dims = self.metadata.get("dimensions")
+        if dims and isinstance(dims, list):
+            for dim in dims:
+                if isinstance(dim, basestring):
+                    err = ValidationError("default", "model", None,
+                                          "dimensions",
+                                          "Dimension '%s' is not described, "
+                                          "creating flat single-attribute "
+                                          "dimension" % dim)
+                    errors.append(err)
+
+        return errors
+
+    def validate_cube(self, cube):
+        validator = jsonschema.Draft4Validator(self.cube_schema)
+        name = cube.get("name")
+
+        return self._collect_errors("cube", name, validator, cube)
+
+    def validate_dimension(self, dim):
+        validator = jsonschema.Draft4Validator(self.dimension_schema)
+        name = dim.get("name")
+
+        errors = self._collect_errors("dimension", name, validator, dim)
+
+        if "default_hierarchy_name" not in dim:
+            error = ValidationError("default", "dimension", name, None,
+                                    "No default hierarchy name specified, "
+                                    "using first one")
+            errors.append(error)
+
+        if "levels" not in dim and "attributes" not in dim:
+            error = ValidationError("default", "dimension", name, None,
+                                    "Neither levels nor attributes specified, "
+                                    "creating flat dimension without details")
+            errors.append(error)
+
+        elif "levels" in dim and "attributes" in dim:
+            error = ValidationError("error", "dimension", name, None,
+                                    "Both levels and attributes specified")
+            errors.append(error)
+
+        return errors
--- cubes/providers.py
+++ cubes/providers.py
@@ -1,12 +1,9 @@
 # -*- coding=utf -*-
 """Logical model model providers."""
-import jsonschema
-import pkgutil
 import copy
 import json
 import re
 
-from collections import namedtuple
 from .logging import get_logger
 from .errors import *
 from .model import *
@@ -16,7 +13,6 @@ from .extensions import Extensible
 __all__ = [
     "ModelProvider",
     "StaticModelProvider",
-    "validate_model",
 
     # FIXME: Depreciated
     "load_model",
@@ -26,7 +22,7 @@ __all__ = [
 
 
 def load_model(resource, translations=None):
-    raise Exception("load_model() was replaced by Workspace.add_model(), "
+    raise Exception("load_model() was replaced by Workspace.import_model(), "
                     "please refer to the documentation for more information")
 
 
@@ -348,105 +344,3 @@ def model_from_path(path):
     """Load logical model from a file or a directory specified by `path`.
     Returs instance of `Model`. """
     raise NotImplementedError("model_from_path is depreciated. use Workspace.add_model()")
-
-
-ValidationError = namedtuple("ValidationError",
-                            ["severity", "scope", "object", "property", "message"])
-
-
-def validate_model(metadata):
-    """Validate model metadata."""
-
-    validator = ModelMetadataValidator(metadata)
-    return validator.validate()
-
-class ModelMetadataValidator(object):
-    def __init__(self, metadata):
-        self.metadata = metadata
-
-        data = pkgutil.get_data("cubes", "schemas/model.json")
-        self.model_schema = json.loads(data)
-
-        data = pkgutil.get_data("cubes", "schemas/cube.json")
-        self.cube_schema = json.loads(data)
-
-        data = pkgutil.get_data("cubes", "schemas/dimension.json")
-        self.dimension_schema = json.loads(data)
-
-    def validate(self):
-        errors = []
-
-        errors += self.validate_model()
-
-        if "cubes" in self.metadata:
-            for cube in self.metadata["cubes"]:
-                errors += self.validate_cube(cube)
-
-        if "dimensions" in self.metadata:
-            for dim in self.metadata["dimensions"]:
-                errors += self.validate_dimension(dim)
-
-        return errors
-
-    def _collect_errors(self, scope, obj, validator, metadata):
-        errors = []
-
-        for error in validator.iter_errors(metadata):
-            if error.path:
-                path = [str(item) for item in error.path]
-                ref = ".".join(path)
-            else:
-                ref = None
-
-            verror = ValidationError("error", scope, obj, ref, error.message)
-            errors.append(verror)
-
-        return errors
-
-    def validate_model(self):
-        validator = jsonschema.Draft4Validator(self.model_schema)
-        errors = self._collect_errors("model", None, validator, self.metadata)
-
-        dims = self.metadata.get("dimensions")
-        if dims and isinstance(dims, list):
-            for dim in dims:
-                if isinstance(dim, basestring):
-                    err = ValidationError("default", "model", None,
-                                          "dimensions",
-                                          "Dimension '%s' is not described, "
-                                          "creating flat single-attribute "
-                                          "dimension" % dim)
-                    errors.append(err)
-
-        return errors
-
-    def validate_cube(self, cube):
-        validator = jsonschema.Draft4Validator(self.cube_schema)
-        name = cube.get("name")
-
-        return self._collect_errors("cube", name, validator, cube)
-
-    def validate_dimension(self, dim):
-        validator = jsonschema.Draft4Validator(self.dimension_schema)
-        name = dim.get("name")
-
-        errors = self._collect_errors("dimension", name, validator, dim)
-
-        if "default_hierarchy_name" not in dim:
-            error = ValidationError("default", "dimension", name, None,
-                                    "No default hierarchy name specified, "
-                                    "using first one")
-            errors.append(error)
-
-        if "levels" not in dim and "attributes" not in dim:
-            error = ValidationError("default", "dimension", name, None,
-                                    "Neither levels nor attributes specified, "
-                                    "creating flat dimension without details")
-            errors.append(error)
-
-        elif "levels" in dim and "attributes" in dim:
-            error = ValidationError("error", "dimension", name, None,
-                                    "Both levels and attributes specified")
-            errors.append(error)
-
-        return errors
