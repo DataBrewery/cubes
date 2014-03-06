395e25305e7efda6d0cf528cec3bbe282fac96ff
Robin Thomas <rthomas@squarespace.com>
2014-03-06 15:33:37 -0500

    undo regex stripping of origin header. read documentation! breathe!

--- cubes/server/blueprint.py
+++ cubes/server/blueprint.py
@@ -16,7 +16,6 @@ from .local import *
 from .auth import NotAuthenticated
 
 from collections import OrderedDict
-import re
 
 from cubes import __version__
 
@@ -542,8 +541,6 @@ def logout():
     else:
         return "logged out"
 
-_http_schemes_pattern = re.compile(r'^https?://', re.IGNORECASE)
-
 @slicer.after_request
 def add_cors_headers(response):
     """Add Cross-origin resource sharing headers."""
@@ -557,8 +554,5 @@ def add_cors_headers(response):
             response.headers['Access-Control-Allow-Headers'] = 'X-Requested-With'
             # OPTIONS preflight requests need to receive origin back instead of wildcard
             if origin == '*':
-                origin_hostname = request.headers.get('Origin', '')
-                origin_hostname = _http_schemes_pattern.sub('', origin_hostname)
-                response.headers['Access-Control-Allow-Origin'] = (origin_hostname or origin)
-
+                response.headers['Access-Control-Allow-Origin'] = request.headers.get('Origin', origin)
     return response
