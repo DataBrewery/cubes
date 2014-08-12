57d15c921acd630924487a8c1ae1393b57ee3712
Robin Thomas <rthomas@squarespace.com>
2014-08-12 14:55:55 -0400

    forgotten import of ModelError

--- cubes/namespace.py
+++ cubes/namespace.py
@@ -2,7 +2,7 @@
 
 from __future__ import absolute_import
 
-from .errors import NoSuchCubeError, NoSuchDimensionError
+from .errors import NoSuchCubeError, NoSuchDimensionError, ModelError
 
 __all__ = [
     "Namespace",
