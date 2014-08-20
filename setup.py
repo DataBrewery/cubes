7f8b5a3b5ea7d089bf1a1df7c101530b5dbe011e
Stefan Urbanek <stefan@agentfarms.net>
2014-08-19 21:56:46 -0400

    Removed base.cubesmodel → now lives in it's own repositorry cubes-models

--- setup.py
+++ setup.py
@@ -15,8 +15,7 @@ setup(
     package_data = {
         # If any package contains *.txt or *.rst files, include them:
         '': ['*.txt', '*.rst'],
-        'cubes': [ 'templates/*.html', 'templates/*.js', 'schemas/*.json',
-                    'models/base.cubesmodel/*'],
+        'cubes': ['templates/*.html', 'templates/*.js', 'schemas/*.json'],
         'cubes.server': ['templates/*.html'],
         'cubes.backends.mixpanel': ['*.json']
     },
