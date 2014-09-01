0e366b5d471262b7b7791c06f2a68be0edca7912
Thomas Levine <_@thomaslevine.com>
2014-09-01 15:16:00 +0000

    making better error messages

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -4,21 +4,22 @@ from cubes.configuration import EnvironmentConfigParser
 from .common import CubesTestCaseBase
 
 
+fn = os.path.join('fixtures', 'procurements.ini')
 class ConfigurationTestCase(CubesTestCaseBase):
     def setUp(self):
         CubesTestCaseBase.setUp(self)
-        fn = os.path.join('fixtures', 'procurements.ini')
         self.c = EnvironmentConfigParser()
-        self.c.read(fn)
         self.url = 'sqlite:///procurements.sqlite'
 
     def test_interpolation(self):
         os.environ['DATABASE_URL'] = self.url
-        self.assertEqual(c['store']['url'], self.url)
+        self.c.read(fn)
+        self.assertEqual(self.c['store']['url'], self.url)
         del(os.environ['DATABASE_URL'])
 
     def test_no_interpolation(self):
-        self.assertEqual(c['store']['type'], 'sql')
+        self.c.read(fn)
+        self.assertEqual(self.c['store']['type'], 'sql')
 
     def test_interpolation_error(self):
         # self.assertRaises
