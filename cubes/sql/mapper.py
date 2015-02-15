b970de405bcb7429f1d7e2511059124728f78d60
Stefan Urbanek <stefan@agentfarms.net>
2015-02-15 00:38:06 -0500

    [sql] added distill_naming

--- cubes/sql/mapper.py
+++ cubes/sql/mapper.py
@@ -26,6 +26,7 @@ from .schema import to_column
 __all__ = (
     "SnowflakeMapper",
     "DenormalizedMapper",
+    "distill_naming",
     "Naming",
     "DEFAULT_KEY_FIELD"
 )
@@ -35,6 +36,9 @@ DEFAULT_KEY_FIELD = "id"
 
 DEFAULT_FACT_KEY = 'id'
 DEFAULT_DIMENSION_KEY = 'id'
+
+# Note: Only keys in this dictionary are allowed in the `naming` dictionary.
+# All other keys are ignored.
 
 NAMING_DEFAULTS = {
     "fact_prefix": None,
@@ -53,7 +57,140 @@ NAMING_DEFAULTS = {
 }
 
 
-# TODO: Use 'Naming' instead of this long list of arguments
+def distill_naming(dictionary):
+    """Distill only keys and values related to the naming conventions."""
+    d = {key: value for key, value
+                    in dictionary.items()
+                    if key in NAMING_DEFAULTS}
+
+    return Naming(d)
+
+
+def _match_names(pattern, names):
+    """Match names to patterns and return a tuple of matching name with
+    extracted value (stripped of suffix/prefix)."""
+
+    result = []
+
+    for name in names:
+        match = pattern.match(name)
+        if match:
+            result.append((name, match.group("name")))
+
+    return result
+
+
+class Naming(AttributeDict):
+    """Naming conventions for SQL tables. Naming properties can be accessed as
+    a dictionary keys or as direct attributes. The naming properties are:
+
+    * `fact_prefix` – prefix for fact tables
+    * `fact_suffix` – suffix for fact tables
+    * `dimension_prefix` – prefix for dimension tables
+    * `dimension_suffix` – suffix for dimension tables
+    * `dimension_key_prefix` – prefix for dimension foreign keys
+    * `dimension_key_suffix` – suffix for dimension foreign keys
+    * `fact_key` – name of fact table primary key (defaults to ``id`` if not
+      specified)
+    * `dimension_key` – name of dimension table primary key (defaults to
+      ``id`` if not specified)
+    * `explicit_dimension_primary` – whether the primary key of dimension
+      table contains dimension name explicitly.
+
+    If the `explicit_dimension_primary` is `True`, then all dimension tables
+    are expected to have the primary key in the same format as foreign
+    dimension keys. For example if the foreign dimension keys are
+    ``customer_key`` then primary key of customer dimension table is also
+    ``customer_key`` as oposed to just ``key``. The `dimension_key` naming
+    property is ignored.
+
+
+    Additional information that can be used by the mapper:
+
+    * `schema` – default schema
+    * `fact_schema` – schema where all fact tables are stored
+    * `dimension_schema` – schema where dimension tables are stored
+
+    Recommended values: `fact_prefix` = ``ft_``, `dimension_prefix` =
+    ``dm_``, `explicit_dimension_primary` = ``True``.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Creates a `Naming` object instance from a dictionary. If `fact_key`
+        or `dimension_key` are not specified, then they are set to ``id`` by
+        default."""
+
+        super(Naming, self).__init__(*args, **kwargs)
+
+        # Set the defaults
+        for key, value in NAMING_DEFAULTS.items():
+            if key not in self:
+                self[key] = value
+
+        pat = re.compile("^{}(?P<name>.*){}$".format(self.dimension_prefix or "",
+                                                 self.dimension_suffix or ""))
+        self["dim_name_pattern"] = pat
+
+        pat = re.compile("^{}(?P<name>.*){}$".format(self.fact_prefix or "",
+                                                 self.fact_suffix or ""))
+        self["fact_name_pattern"] = pat
+
+        pat = re.compile("^{}(?P<name>.*){}$".format(self.dimension_key_prefix or "",
+                                                 self.dimension_key_suffix or ""))
+        self["dim_key_pattern"] = pat
+
+    def dimension_table_name(self, name):
+        """Constructs a physical dimension table name for dimension `name`"""
+
+        table_name = "{}{}{}".format(
+                            self.dimension_prefix or "",
+                            name,
+                            self.dimension_suffix or "")
+        return table_name
+
+    def fact_table_name(self, name):
+        """Constructs a physical fact table name for fact/cube `name`"""
+
+        table_name = "{}{}{}".format(
+                            self.fact_prefix or "",
+                            name,
+                            self.fact_suffix or "")
+        return table_name
+
+    def dimension_primary_key(self, name):
+        """Constructs a dimension primary key name for dimension `name`"""
+
+        if self.explicit_dimension_primary:
+            key = "{}{}{}".format(
+                    self.dimension_key_prefix or "",
+                    name,
+                    self.dimension_key_suffix or "")
+            return key
+        else:
+            return self.dimension_key
+
+    def dimension_keys(self, keys):
+        """Return a list of tuples (`key`, `dimension`) for every key in
+        `keys` that matches dimension key naming. Useful when trying to
+        identify dimensions and their foreign keys in a fact table that
+        follows the naming convetion."""
+
+        return _match_names(self.dim_key_pattern, keys)
+
+    def dimensions(self, table_names):
+        """Return a list of tuples (`table`, `dimension`) for all tables that
+        match dimension naming scheme. Usefult when trying to identify
+        dimension tables in a database that follow the naming convention."""
+
+        return _match_names(self.dim_name_pattern, table_names)
+
+    def facts(self, table_names):
+        """Return a list of tuples (`table`, `fact`) for all tables that
+        match fact table naming scheme. Useful when trying to identify fact
+        tables in a database that follow the naming convention."""
+
+        return _match_names(self.fact_name_pattern, table_names)
+
 
 class SnowflakeMapper(Mapper):
     """Mapper is core clas for translating logical model to physical
@@ -109,7 +246,7 @@ class SnowflakeMapper(Mapper):
 
         self.mappings = mappings or cube.mappings
 
-        self.naming = Naming(naming)
+        self.naming = distill_naming(naming)
 
         if not (fact_name or self.cube.fact or self.cube.basename):
             raise ModelError("Can not determine cube fact name")
@@ -267,136 +404,3 @@ class DenormalizedMapper(Mapper):
         return reference
 
 
-class Naming(AttributeDict):
-    """Naming conventions for SQL tables. Naming properties can be accessed as
-    a dictionary keys or as direct attributes. The naming properties are:
-
-    * `fact_prefix` – prefix for fact tables
-    * `fact_suffix` – suffix for fact tables
-    * `dimension_prefix` – prefix for dimension tables
-    * `dimension_suffix` – suffix for dimension tables
-    * `dimension_key_prefix` – prefix for dimension foreign keys
-    * `dimension_key_suffix` – suffix for dimension foreign keys
-    * `fact_key` – name of fact table primary key (defaults to ``id`` if not
-      specified)
-    * `dimension_key` – name of dimension table primary key (defaults to
-      ``id`` if not specified)
-    * `explicit_dimension_primary` – whether the primary key of dimension
-      table contains dimension name explicitly.
-
-    If the `explicit_dimension_primary` is `True`, then all dimension tables
-    are expected to have the primary key in the same format as foreign
-    dimension keys. For example if the foreign dimension keys are
-    ``customer_key`` then primary key of customer dimension table is also
-    ``customer_key`` as oposed to just ``key``. The `dimension_key` naming
-    property is ignored.
-
-
-    Additional information that can be used by the mapper:
-
-    * `schema` – default schema
-    * `fact_schema` – schema where all fact tables are stored
-    * `dimension_schema` – schema where dimension tables are stored
-
-    Recommended values: `fact_prefix` = ``ft_``, `dimension_prefix` =
-    ``dm_``, `explicit_dimension_primary` = ``True``.
-
-    """
-
-    def __init__(self, *args, **kwargs):
-        """Creates a `Naming` object instance from a dictionary. If `fact_key`
-        or `dimension_key` are not specified, then they are set to ``id`` by
-        default."""
-
-        super(Naming, self).__init__(*args, **kwargs)
-
-        # Set the defaults
-        for key, value in NAMING_DEFAULTS.items():
-            if key not in self:
-                self[key] = value
-
-        pat = re.compile("^{}(?P<name>.*){}$".format(self.dimension_prefix or "",
-                                                 self.dimension_suffix or ""))
-        self["dim_name_pattern"] = pat
-
-        pat = re.compile("^{}(?P<name>.*){}$".format(self.fact_prefix or "",
-                                                 self.fact_suffix or ""))
-        self["fact_name_pattern"] = pat
-
-        pat = re.compile("^{}(?P<name>.*){}$".format(self.dimension_key_prefix or "",
-                                                 self.dimension_key_suffix or ""))
-        self["dim_key_pattern"] = pat
-
-    def dimension_table_name(self, name):
-        """Constructs a physical dimension table name for dimension `name`"""
-
-        table_name = "{}{}{}".format(
-                            self.dimension_prefix or "",
-                            name,
-                            self.dimension_suffix or "")
-        return table_name
-
-    def fact_table_name(self, name):
-        """Constructs a physical fact table name for fact/cube `name`"""
-
-        table_name = "{}{}{}".format(
-                            self.fact_prefix or "",
-                            name,
-                            self.fact_suffix or "")
-        return table_name
-
-    def dimension_primary_key(self, name):
-        """Constructs a dimension primary key name for dimension `name`"""
-
-        if self.explicit_dimension_primary:
-            key = "{}{}{}".format(
-                    self.dimension_key_prefix or "",
-                    name,
-                    self.dimension_key_suffix or "")
-            return key
-        else:
-            return self.dimension_key
-
-    def dimension_keys(self, keys):
-        """Return a list of tuples (`key`, `dimension`) for every key in
-        `keys` that matches dimension key naming. Useful when trying to
-        identify dimensions and their foreign keys in a fact table that
-        follows the naming convetion."""
-
-        result = []
-
-        for key in keys:
-            match = self.dim_key_pattern.match(key)
-            if match:
-                result.append((key, match.group("name")))
-
-
-        return result
-
-    def dimensions(self, table_names):
-        """Return a list of tuples (`table`, `dimension`) for all tables that
-        match dimension naming scheme. Usefult when trying to identify
-        dimension tables in a database that follow the naming convention."""
-
-        result = []
-
-        for name in table_names:
-            match = self.dim_name_pattern.match(name)
-            if match:
-                result.append((name, match.group("name")))
-
-        return result
-
-    def facts(self, table_names):
-        """Return a list of tuples (`table`, `fact`) for all tables that
-        match fact table naming scheme. Useful when trying to identify fact
-        tables in a database that follow the naming convention."""
-
-        result = []
-
-        for name in table_names:
-            match = self.fact_name_pattern.match(name)
-            if match:
-                result.append((name, match.group("name")))
-
-        return result
