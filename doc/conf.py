2c1593712308ed1bf2c1944b265d77cb623c2cba
Stefan Urbanek <stefan@agentfarms.net>
2015-04-07 22:22:14 -0400

    Use default doc theme

--- doc/conf.py
+++ doc/conf.py
@@ -95,7 +95,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "classic"
+# html_theme = "classic"
 
 # html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
