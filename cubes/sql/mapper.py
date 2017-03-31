4597a795efac0d0251d48d3475a0e5835a5c0362
Stefan Urbanek <stefan@agentfarms.net>
2017-03-31 08:30:26 -0700

    Changed naming propertie to be first class
    Naming is no longer a dictionary with magical attributes/getters but a
    real object with properties for every naming component.

--- cubes/sql/mapper.py
+++ cubes/sql/mapper.py
@@ -12,8 +12,11 @@ from typing import (
         Pattern,
         Tuple,
         Type,
+        TypeVar,
+        Union,
     )
 
+from collections import defaultdict
 
 from ..types import JSONType
 
@@ -100,8 +103,14 @@ def _match_names(pattern: Pattern, names: Collection[str]) \
 
     return result
 
+NamingDict = Dict[str, Union[str, bool, None]]
+T = TypeVar("T", Optional[str], Optional[bool])
 
-class Naming(AttributeDict):
+def _naming_default(naming: NamingDict, key: str) -> T:
+    return naming.get(key, NAMING_DEFAULTS.get(key))
+
+
+class Naming:
     """Naming conventions for SQL tables. Naming properties can be accessed as
     a dictionary keys or as direct attributes. The naming properties are:
 
@@ -136,29 +145,55 @@ class Naming(AttributeDict):
     ``dm_``, `explicit_dimension_primary` = ``True``.
     """
 
-    def __init__(self, *args: str, **kwargs: str) -> None:
+    fact_prefix: Optional[str]
+    fact_suffix: Optional[str]
+    dimension_prefix: Optional[str]
+    dimension_suffix: Optional[str]
+    dimension_key_prefix: Optional[str]
+    dimension_key_suffix: Optional[str]
+    fact_key: Optional[str]
+    dimension_key: Optional[str]
+    explicit_dimension_primary: bool
+    schema: Optional[str]
+    fact_schema: Optional[str]
+    dimension_schema: Optional[str]
+
+    dim_name_pattern: Pattern
+    fact_name_pattern: Pattern
+    dim_key_pattern: Pattern
+
+    def __init__(self, naming: NamingDict) -> None:
         """Creates a `Naming` object instance from a dictionary. If `fact_key`
         or `dimension_key` are not specified, then they are set to ``id`` by
         default."""
 
-        super(Naming, self).__init__(*args, **kwargs)
+        def get_default(key: str) -> Union[str, bool, None]:
+            return NAMING_DEFAULTS.get(key)
 
-        # Set the defaults
-        for key, value in NAMING_DEFAULTS.items():
-            if key not in self:
-                self[key] = value
+        self.fact_prefix = _naming_default(naming, "fact_prefix")
+        self.fact_suffix = _naming_default(naming, "fact_suffix")
+        self.dimension_prefix = _naming_default(naming, "dimension_prefix")
+        self.dimension_suffix = _naming_default(naming, "dimension_suffix")
+        self.dimension_key_prefix = _naming_default(naming, "dimension_key_prefix")
+        self.dimension_key_suffix = _naming_default(naming, "dimension_key_suffix")
+        self.fact_key = _naming_default(naming, "fact_key")
+        self.dimension_key = _naming_default(naming, "dimension_key")
+        self.explicit_dimension_primary = _naming_default(naming, "explicit_dimension_primary")
+        self.schema = _naming_default(naming, "schema")
+        self.fact_schema = _naming_default(naming, "fact_schema")
+        self.dimension_schema = _naming_default(naming, "dimension_schema")
 
-        pat = re.compile("^{}(?P<name>.*){}$"
-                         .format(self.dimension_prefix or "", self.dimension_suffix or ""))
-        self["dim_name_pattern"] = pat
+        dim_name_pattern = re.compile("^{}(?P<name>.*){}$"
+                                      .format(self.dimension_prefix or "",
+                                              self.dimension_suffix or ""))
 
-        pat = re.compile("^{}(?P<name>.*){}$"
-                         .format(self.fact_prefix or "", self.fact_suffix or ""))
-        self["fact_name_pattern"] = pat
+        fact_name_pattern = re.compile("^{}(?P<name>.*){}$"
+                                       .format(self.fact_prefix or "",
+                                       self.fact_suffix or ""))
 
-        pat = re.compile("^{}(?P<name>.*){}$"
-                         .format(self.dimension_key_prefix or "", self.dimension_key_suffix or ""))
-        self["dim_key_pattern"] = pat
+        dim_key_pattern = re.compile("^{}(?P<name>.*){}$"
+                                     .format(self.dimension_key_prefix or "",
+                                             self.dimension_key_suffix or ""))
 
     def dimension_table_name(self, name: str) -> str:
         """Constructs a physical dimension table name for dimension `name`"""
